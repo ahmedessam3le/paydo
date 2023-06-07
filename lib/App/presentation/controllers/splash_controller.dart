b011af32688b84c921ebfd699aa3d8919d454aaa
ahmedessam3le <ahmedessam3le@gmail.com>
2023-06-08 00:15:20 +0300

    feat: Splash Presentation Added

--- lib/App/presentation/controllers/splash_controller.dart
+++ lib/App/presentation/controllers/splash_controller.dart
@@ -1,3 +1,19 @@
 import 'package:get/get.dart';
 
-class SplashController extends GetxController {}
+import '../../../core/utils/app_router.dart';
+import '../../../core/utils/app_utils.dart';
+
+class SplashController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+    goNext();
+  }
+
+  void goNext() async {
+    await AppUtils.delay(milliseconds: 2000);
+    navigateToTransactions();
+  }
+
+  void navigateToTransactions() => Get.offAllNamed(AppRoutes.transactionsRoute);
+}
--- lib/App/presentation/views/splash_view.dart
+++ lib/App/presentation/views/splash_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../../core/const/app_strings.dart';
+import '../../../core/style/app_colors.dart';
 import '../controllers/splash_controller.dart';
 
 class SplashView extends GetView<SplashController> {
@@ -8,6 +10,38 @@ class SplashView extends GetView<SplashController> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+      color: Get.theme.scaffoldBackgroundColor,
+      child: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 100,
+                backgroundColor: AppColors.primary,
+                child: Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Container(
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(shape: BoxShape.circle),
+                    child: Image.asset(
+                      'assets/images/app_icon.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 35),
+              Text(
+                AppStrings.appName,
+                textAlign: TextAlign.center,
+                style: Get.textTheme.titleLarge,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
